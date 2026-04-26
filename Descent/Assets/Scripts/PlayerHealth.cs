c5fb3f2007ca45a6db494562f823542c975c5d4c
Christian Barajas <cbar@csu.fullerton.edu>
2026-04-25 23:45:39 -0700

    Add player death restart system

--- Descent/Assets/Scripts/PlayerHealth.cs
+++ Descent/Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -45,6 +46,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
-        Debug.Log("Player died!");
+        Debug.Log("Player died! Restarting scene...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
